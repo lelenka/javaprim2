77e42e9d12b6b3dbfb08df4d83709e5f8167ecd2
Olga rybak <lelenka2022@mail.ru>
2024-08-23 16:29:39 +0300

    added files

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+        public class Main {
+            public static void main(String[] args) {
+
+                int userBalance = 100;
+                int added = 300;
+                int bonus = 0;
+
+                if (added > 1000)
+                {
+                    bonus = (added / 100);
+                }
+                else {
+                    bonus = 0;}
+                int summary;
+                summary = userBalance + added + bonus;
+                System.out.println (" Бонус: " + bonus);
+                System.out.println (" Итоговая сумма на счету клиента: " + summary);
+    }
+}
+
